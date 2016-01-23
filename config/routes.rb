093a80db396aa8f4814596e5301fe16429afd2ba
1UnboundedSentience <jsnx21@gmail.com>
2016-01-23 15:30:38 -0800

    map.html on my branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
+  get 'static_pages/map'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
