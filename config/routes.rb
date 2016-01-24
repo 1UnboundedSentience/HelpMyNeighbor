c48cff697020ed77961e9729d433774290e23833
Nolan Frazier <nofrazier3@gmail.com>
2016-01-23 19:58:05 -0800

    Complete shelter pg

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   root 'static_pages#home'
+  get '/shelter' => 'static_pages#shelter'
 end
